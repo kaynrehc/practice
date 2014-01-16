5d09933703a1f3c5a3ae9b45fe674dcf8417fd83
Michael J. Chernyak <kaynrehc@gmail.com>
2014-01-15 20:55:15 -0500

    Future play

--- threadplay/src/main/java/net/jcip/examples/Preloader.java
+++ threadplay/src/main/java/net/jcip/examples/Preloader.java
@@ -2,44 +2,84 @@ package net.jcip.examples;
 
 import java.util.concurrent.*;
 
-/**
- * Preloader
- *
- * Using FutureTask to preload data that is needed later
- *
- * @author Brian Goetz and Tim Peierls
- */
-
 public class Preloader {
-    ProductInfo loadProductInfo() throws DataLoadException {
-        return null;
-    }
 
-    private final FutureTask<ProductInfo> future =
-        new FutureTask<ProductInfo>(new Callable<ProductInfo>() {
-            public ProductInfo call() throws DataLoadException {
-                return loadProductInfo();
-            }
-        });
-    private final Thread thread = new Thread(future);
+	private final ProductInfoCreator productInfoCreator = new ProductInfoCreator();
+	private final FutureTask<ProductInfo> productInfoFutureTask = new FutureTask<ProductInfo>(productInfoCreator);
 
-    public void start() { thread.start(); }
+	/**
+	 * A Callable implementation, this class's call() method creates a new ProductInfo object.
+	 */
+	private class ProductInfoCreator implements Callable<ProductInfo> {
+		public ProductInfo call() throws DataLoadException {
+			ProductInfo productInfo = new ProductInfo();
+			long milli = 0;
+			while ((milli = System.currentTimeMillis()) % 10000 != 0) ;
+			productInfo.setName("[PI_" + milli + "]");
+			return productInfo;
+		}
+	}
 
-    public ProductInfo get()
-            throws DataLoadException, InterruptedException {
-        try {
-            return future.get();
-        } catch (ExecutionException e) {
-            Throwable cause = e.getCause();
-            if (cause instanceof DataLoadException)
-                throw (DataLoadException) cause;
-            else
-                throw LaunderThrowable.launderThrowable(cause);
-        }
-    }
+	public void start() {
+		System.out.println("***** Preloader.start()");
+		new Thread(productInfoFutureTask).start();
+	}
 
-    interface ProductInfo {
-    }
+	public ProductInfo get()
+			throws DataLoadException, InterruptedException {
+
+		System.out.println("***** Preloader.get()");
+
+		try {
+			System.out.println("***** calling productInfoFutureTask.get()");
+			return productInfoFutureTask.get();
+		} catch (ExecutionException e) {
+			Throwable cause = e.getCause();
+			if (cause instanceof DataLoadException)
+				throw (DataLoadException) cause;
+			else
+				throw LaunderThrowable.launderThrowable(cause);
+		}
+	}
+
+	class ProductInfo {
+		String name;
+
+		String getName() {
+			return name;
+		}
+
+		void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "ProductInfo{" +
+					"name='" + name + '\'' +
+					'}';
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("***** main() start");
+
+		Preloader preloader = new Preloader();
+		preloader.start();
+
+		ProductInfo pi = null;
+		try {
+			pi = preloader.get();
+		} catch (DataLoadException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("***** " + pi);
+		System.out.println("***** main() finish");
+	}
 }
 
-class DataLoadException extends Exception { }
+class DataLoadException extends Exception {
+}
